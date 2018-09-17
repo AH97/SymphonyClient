ecd4e75e310e34a9835a7d3befddbbd4909a1378
AH97 <me@alexharrop.net>
2018-09-17 16:16:30 -0400

    music playing in browser

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,15 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
     res.render('index/index', {
         title: 'Symphony Streaming',
-        name: req.params.name
+        name: req.body.name
     });
 });
+
+router.post('/', function (req, res, next) {
+    res.render('index/index', {
+        title: req.body.name,
+        name: req.body.name
+    })
+})
 
 module.exports = router;
